0131651697b14ffa23246ba458038afc2f1a650b
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-05-24 00:22:25 -0300

    test: começando o teste do Amigo

--- src/test/java/modelo/TestAmigo.java
+++ src/test/java/modelo/TestAmigo.java
@@ -4,10 +4,32 @@
  */
 package modelo;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 /**
  *
  * @author mamaa
  */
 public class TestAmigo {
     
+	Amigo instancia = new Amigo();
+    
+    /**
+     * Testa o construtor sem argumentos do Amigo.
+     */
+    @Test
+    public void testAmigo(){
+        assertTrue((instancia.getIdAmigo() == 0) && "".equals(instancia.getNomeAmigo()) && "".equals(instancia.getTelefoneAmigo()) && "".equals(instancia.getEmailAmigo()));
+    }
+    
+    @Test
+    public void testIdAmigoInt(){
+        instancia.setIdAmigo(1);
+        assertTrue((instancia.getIdAmigo() == 1) && "".equals(instancia.getNomeAmigo()) && "".equals(instancia.getTelefoneAmigo()) && "".equals(instancia.getEmailAmigo()));
+    }
 }
