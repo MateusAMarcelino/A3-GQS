cc1e15eb16412335a291047318c507535ab9792e
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-05-26 11:00:32 -0300

    test: continuando os testes do Amigo

--- src/test/java/modelo/TestAmigo.java
+++ src/test/java/modelo/TestAmigo.java
@@ -1,6 +1,8 @@
 package modelo;
 
 
+import dao.AmigoDAO;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,5 +28,41 @@ public class TestAmigo {
         instancia.setTelefoneAmigo("");
         instancia.setEmailAmigo("");
         assertTrue((instancia.getIdAmigo() == 1) && "".equals(instancia.getNomeAmigo()) && "".equals(instancia.getTelefoneAmigo()) && "".equals(instancia.getEmailAmigo()));
+    }
+    
+    @Test
+    public void testListaAmigo(){
+        Amigo amigo = new Amigo(new AmigoFake());
+        ArrayList<Amigo> lista = amigo.ListaAmigo();
+
+        assertNotNull(lista);
+        assertEquals(1, lista.size());
+        assertEquals("Osmar", lista.get(0).getNomeAmigo());
+    }
+    
+    @Test
+    void testInsertAmigoBD() {
+        AmigoFake fake = new AmigoFake();
+        Amigo amigo = new Amigo(fake);
+
+        boolean resultado = amigo.insertAmigoBD("Osmar", "123456", "Osmar@gmail.com");
+
+        assertTrue(resultado);
+        assertEquals(1, fake.chamaMaiorID);
+        assertNotNull(fake.ultimoAmigoInserido);
+        assertEquals(2, fake.ultimoAmigoInserido.getIdAmigo());
+        assertEquals("Osmar", fake.ultimoAmigoInserido.getNomeAmigo());
+    }
+
+    @Test
+    void testDeleteAmigoBD(){
+        AmigoFake fake = new AmigoFake();
+        Amigo amigo = new Amigo(fake);
+        
+        boolean resultado = amigo.deleteAmigoBD(99);
+        
+        assertTrue(resultado);
+        assertTrue(fake.foiChamado);
+        assertEquals(99, fake.ultimoIdDeletado);
     }
 }
