31fbbfc87c9274a13103ee11efe5335eeb8771ab
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-05-28 14:56:10 -0300

    feat: Acrescentando mais métodos ao AmigoFake para os testes

--- src/test/java/modelo/AmigoFake.java
+++ src/test/java/modelo/AmigoFake.java
@@ -4,17 +4,13 @@ import dao.AmigoDAO;
 import java.util.ArrayList;
 
 /**
- *
  * Classe que cria um amigo falso para ser utilizado nos testes.
- * 
  * @author lorenzoeid
  */
 public class AmigoFake extends AmigoDAO {
     
     /**
-     * 
      * Sobrescreve a ArrayList para o teste
-     * 
      */
     @Override
     public ArrayList<Amigo> getListaAmigo(){
@@ -24,9 +20,7 @@ public class AmigoFake extends AmigoDAO {
     }
     
     /**
-     * 
      * Sobrescreve o método maiorIDAmigo para o teste
-     * 
      */
     public int chamaMaiorID = 0;
     public Amigo ultimoAmigoInserido = null;
@@ -38,9 +32,7 @@ public class AmigoFake extends AmigoDAO {
     }
 
     /**
-     * 
      * Sobrescreve o método insertAmigoBD para o teste
-     * 
      */
     @Override
     public boolean insertAmigoBD(Amigo amigo) {
@@ -49,9 +41,7 @@ public class AmigoFake extends AmigoDAO {
     }
     
     /**
-     * 
      * Sobrescreve o método deleteAmigoBD para o teste
-     * 
      */
     public int ultimoIdDeletado = -1;
     public boolean foiChamado = false;
@@ -61,5 +51,29 @@ public class AmigoFake extends AmigoDAO {
         ultimoIdDeletado = IdAmigo;
         foiChamado = true;
         return true;
+    }
+    
+    /**
+     * Sobrescreve o método updateAmigoBD para o teste
+     */
+    public Amigo ultimoAmigoAtualizado;
+    public boolean metodoChamado = false;
+    
+    @Override
+    public boolean updateAmigoBD(Amigo amigo) {
+        this.ultimoAmigoAtualizado = amigo;
+        this.metodoChamado = true;
+        return true;
+    }
+    
+    /**
+     * Sobrescreve o método recuperaAmigoDB
+     */
+    public int idRecebido;
+    
+    @Override
+    public Amigo RecuperaAmigoDB(int id) {
+        this.idRecebido = id;
+        return new Amigo(id, "Osmar", "123456", "Osmar@gmail.com");
     }
 }
