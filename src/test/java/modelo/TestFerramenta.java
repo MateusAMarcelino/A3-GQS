0f48cbd920730ba9664a386edbf58fde09c40320
guiho <guiho@192.168.0.6>
2025-05-27 23:09:23 -0300

    test: arrumando documentação tirando os testes com DAO

--- src/test/java/modelo/TestFerramenta.java
+++ src/test/java/modelo/TestFerramenta.java
@@ -49,23 +49,4 @@ public class TestFerramenta {
         assertEquals(25.50, ferramenta.getCustoFerramentas(), 0.01);
     }
 
-    /**
-     * Testa se a ferramenta é marcada como disponível quando não está em empréstimo.
-     * Este teste assume que não há empréstimos ativos no momento.
-     */
-    @Test
-    public void testDisponibilidadeFerramentaSemEmprestimo() {
-        // O método depende do EmpréstimoDAO
-        assertTrue(ferramenta.getDisponibilidadeFerramenta(1));
-    }
-
-    /**
-     * Testa se a ferramenta é coolocada corretamente no banco de dados.
-     * Este teste depende de uma implementação do DAO (depois olhar o DAO).
-     */
-    @Test
-    public void testColocaFerramentaDB() {
-        boolean colocado = ferramenta.InsertFerramentaDB("Alicate", "Bosch", 29.99);
-        assertTrue(colocado);
-    }
 }
