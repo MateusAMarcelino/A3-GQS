8784cd1f35c822505feab88366c97ea2c688c2c2
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2025-05-29 10:17:40 -0300

    fix : resolvendo issues do EmprestimoDAO

--- src/main/java/dao/EmprestimoDAO.java
+++ src/main/java/dao/EmprestimoDAO.java
@@ -29,21 +29,20 @@ public class EmprestimoDAO {
        ListaEmprestimo.clear(); //Limpa nosso ArrayList
        
        try{
-           Statement stmt = ut.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_emprestimos");
-            while (res.next()) {
-
-                int IdEmprestimo = res.getInt("IdEmprestimo");
-                int IdAmigo = res.getInt("IdAmigo");
-                int IdFerramentas = res.getInt("IdFerramentas");
-                String dataEmp = res.getString("DataEmprestimo");
-                String dataDev = res.getString("DataDevolucao");
-
-                Emprestimo objeto = new Emprestimo(IdEmprestimo, IdAmigo, IdFerramentas, dataEmp, dataDev);
-
-                ListaEmprestimo.add(objeto);
-       }
-            stmt.close();
+           try (Statement stmt = ut.getConexao().createStatement()) {
+               ResultSet res = stmt.executeQuery("SELECT * FROM tb_emprestimos");
+               while (res.next()) {
+                   
+                   int IdEmprestimo = res.getInt("IdEmprestimo");
+                   int IdAmigo = res.getInt("IdAmigo");
+                   int IdFerramentas = res.getInt("IdFerramentas");
+                   String dataEmp = res.getString("DataEmprestimo");
+                   String dataDev = res.getString("DataDevolucao");
+                   
+                   Emprestimo objeto = new Emprestimo(IdEmprestimo, IdAmigo, IdFerramentas, dataEmp, dataDev);
+                   
+                   ListaEmprestimo.add(objeto);
+               }   }
     } catch (SQLException ex){
         System.out.println("Erro: " + ex);
     }
@@ -65,11 +64,11 @@ public class EmprestimoDAO {
      public int maiorIDEmprestimo() {
         int maiorIdEmprestimo = 0;
         try {
-            Statement stmt = ut.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT MAX(IdEmprestimo) IdEmprestimo FROM tb_emprestimos");
-            res.next();
-            maiorIdEmprestimo = res.getInt("IdEmprestimo");
-            stmt.close();
+            try (Statement stmt = ut.getConexao().createStatement()) {
+                ResultSet res = stmt.executeQuery("SELECT MAX(IdEmprestimo) IdEmprestimo FROM tb_emprestimos");
+                res.next();
+                maiorIdEmprestimo = res.getInt("IdEmprestimo");
+            }
         } catch (SQLException ex) {
             System.out.println("Erro:" + ex);
         }
@@ -85,16 +84,15 @@ public class EmprestimoDAO {
      public boolean insertEmprestimoBD(Emprestimo objeto) {
         String sql = "INSERT INTO tb_emprestimos(IdEmprestimo,IdAmigo,IdFerramentas,DataEmprestimo,DataDevolucao) VALUES(?,?,?,?,?)";
         try {
-            PreparedStatement stmt = ut.getConexao().prepareStatement(sql);
-
-            stmt.setInt(1, objeto.getIdEmprestimo());
-            stmt.setInt(2, objeto.getIdAmigo());
-            stmt.setInt(3, objeto.getIdFerramentas());
-            stmt.setString(4, objeto.getDataEmp());
-            stmt.setString(5, objeto.getDataDev());
-
-            stmt.execute();
-            stmt.close();
+            try (PreparedStatement stmt = ut.getConexao().prepareStatement(sql)) {
+                stmt.setInt(1, objeto.getIdEmprestimo());
+                stmt.setInt(2, objeto.getIdAmigo());
+                stmt.setInt(3, objeto.getIdFerramentas());
+                stmt.setString(4, objeto.getDataEmp());
+                stmt.setString(5, objeto.getDataDev());
+                
+                stmt.execute();
+            }
 
             return true;
         } catch (SQLException erro) {
@@ -110,9 +108,9 @@ public class EmprestimoDAO {
       */
      public boolean deleteEmprestimoBD(int idEmprestimo) {
         try {
-            Statement stmt = ut.getConexao().createStatement();
-            stmt.executeUpdate("DELETE FROM tb_emprestimos WHERE IdEmprestimo = " + idEmprestimo);
-            stmt.close();
+            try (Statement stmt = ut.getConexao().createStatement()) {
+                stmt.executeUpdate("DELETE FROM tb_emprestimos WHERE IdEmprestimo = " + idEmprestimo);
+            }
 
         } catch (SQLException erro) {
             System.out.println("Erro:" + erro);
@@ -130,15 +128,15 @@ public class EmprestimoDAO {
          Emprestimo emprestimo = new Emprestimo();
          emprestimo.setIdEmprestimo(IdEmprestimo);
          try {
-             Statement smt = ut.getConexao().createStatement();
-            ResultSet res = smt.executeQuery("select * from tb_emprestimo where IdEmprestimo = " + IdEmprestimo);
-            res.next();
-            emprestimo.setIdEmprestimo(res.getInt("IdEmprestimo"));
-            emprestimo.setDataEmp(res.getString("DataEmprestimo"));
-            emprestimo.setDataDev(res.getString("DataDevolucao"));
-            emprestimo.setIdAmigo(res.getInt("IdAmigo"));
-            emprestimo.setIdFerramentas(res.getInt("IdFerramentas"));
-            smt.close();
+             try (Statement smt = ut.getConexao().createStatement()) {
+                 ResultSet res = smt.executeQuery("select * from tb_emprestimo where IdEmprestimo = " + IdEmprestimo);
+                 res.next();
+                 emprestimo.setIdEmprestimo(res.getInt("IdEmprestimo"));
+                 emprestimo.setDataEmp(res.getString("DataEmprestimo"));
+                 emprestimo.setDataDev(res.getString("DataDevolucao"));
+                 emprestimo.setIdAmigo(res.getInt("IdAmigo"));
+                 emprestimo.setIdFerramentas(res.getInt("IdFerramentas"));
+             }
         } catch (SQLException erro) {
             System.out.println("Erro: " + erro);
         }
@@ -154,22 +152,22 @@ public class EmprestimoDAO {
     String sql = "UPDATE tb_emprestimos SET IdAmigo=?, IdFerramentas=?, DataEmprestimo=?, DataDevolucao=? WHERE IdEmprestimo=?";
 
     try {
-        PreparedStatement stmt = ut.getConexao().prepareStatement(sql);
-        stmt.setInt(1, objeto.getIdAmigo());
-        stmt.setInt(2, objeto.getIdFerramentas());
-        stmt.setString(3, objeto.getDataEmp());
-        
-        // Verifica se a data de devolução de um emprestimo é nula ou não.
-        if (objeto.getDataDev() == null || objeto.getDataDev().isEmpty()) {
-            stmt.setNull(4, Types.DATE); // Caso seja vazia, define a mesma como nula.
-        } else {
-            stmt.setString(4, objeto.getDataDev()); // Caso não, define a data de devolução.
+        try (PreparedStatement stmt = ut.getConexao().prepareStatement(sql)) {
+            stmt.setInt(1, objeto.getIdAmigo());
+            stmt.setInt(2, objeto.getIdFerramentas());
+            stmt.setString(3, objeto.getDataEmp());
+            
+            // Verifica se a data de devolução de um emprestimo é nula ou não.
+            if (objeto.getDataDev() == null || objeto.getDataDev().isEmpty()) {
+                stmt.setNull(4, Types.DATE); // Caso seja vazia, define a mesma como nula.
+            } else {
+                stmt.setString(4, objeto.getDataDev()); // Caso não, define a data de devolução.
+            }
+            
+            stmt.setInt(5, objeto.getIdEmprestimo());
+            
+            stmt.executeUpdate();
         }
-
-        stmt.setInt(5, objeto.getIdEmprestimo());
-
-        stmt.executeUpdate();
-        stmt.close();
 
         return true;
     } catch (SQLException erro) {
