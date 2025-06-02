8a9813d324c2e58762280c5ceab813d7655a2ac8
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2025-06-02 12:35:58 -0300

    fix : substituindo o system.out pelo logger no AmigoDAO

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -6,49 +6,51 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import modelo.Amigo;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AmigoDAO {
-    
+
     /*
     Inicia o Utilitario,realizando a conecxão com o DB.
-    */
-
+     */
     private Utilitario ut = new Utilitario();
     private String Erro = "Erro:";
-    
+    private static final Logger LOGGER = Logger.getLogger(AmigoDAO.class.getName());
+
     /**
      * Cria a ArrayList para os amigos.
      */
     public static ArrayList<Amigo> ListaAmigo = new ArrayList<>();
-    
-    
+
     /**
-     * Coleta as informações dos amigos do banco de dados e cria um objeto com essas informações no ArrayList da ListaAmigos. 
+     * Coleta as informações dos amigos do banco de dados e cria um objeto com
+     * essas informações no ArrayList da ListaAmigos.
+     *
      * @return A lista de ferramentas após pegar os dados.
      */
-     public ArrayList<Amigo> getListaAmigo() {
+    public ArrayList<Amigo> getListaAmigo() {
 
         ListaAmigo.clear(); // Limpa nosso ArrayList.
-                 
-       
+
         try {
             try (Statement stmt = ut.getConexao().createStatement()) {
                 ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos");
                 while (res.next()) {
-                    
+
                     int IdAmigo = res.getInt("IdAmigo");
                     String NomeAmigo = res.getString("NomeAmigo");
                     String TelefoneAmigo = res.getString("TelefoneAmigo");
                     String EmailAmigo = res.getString("EmailAmigo");
-                    
+
                     Amigo objeto = new Amigo(IdAmigo, NomeAmigo, TelefoneAmigo, EmailAmigo);
-                    
+
                     ListaAmigo.add(objeto);
                 }
             }
 
         } catch (SQLException ex) {
-            System.out.println(Erro + ex);
+            LOGGER.log(Level.SEVERE, "Erro ao acessar o banco de dados", ex);
         }
         return ListaAmigo;
     }
@@ -57,10 +59,12 @@ public class AmigoDAO {
         AmigoDAO.ListaAmigo = ListaAmigo;
     }
 
-   /**
-    * Procura o maior ID entre os amigos, buscando entre todos os amigos no banco de dados.
-    * @return O maior ID encontrado.
-    */
+    /**
+     * Procura o maior ID entre os amigos, buscando entre todos os amigos no
+     * banco de dados.
+     *
+     * @return O maior ID encontrado.
+     */
     public int maiorIDAmigo() {
         int maiorID = 0;
         try {
@@ -70,15 +74,18 @@ public class AmigoDAO {
                 maiorID = res.getInt("IdAmigo");
             }
         } catch (SQLException ex) {
-            System.out.println(Erro + ex);
+            LOGGER.log(Level.SEVERE, "Erro ao localizar o Maior ID", ex);
         }
         return maiorID;
     }
-    
+
     /**
      * Insere as informações como um amigo no banco de dados.
-     * @param objeto é o objeto de amigos, que carrega as informações, como id, nome, etc..
-     * @return True, caso seja possivel inserir o amigo, retornando um erro caso não seja possivel.
+     *
+     * @param objeto é o objeto de amigos, que carrega as informações, como id,
+     * nome, etc..
+     * @return True, caso seja possivel inserir o amigo, retornando um erro caso
+     * não seja possivel.
      */
     public boolean insertAmigoBD(Amigo objeto) {
         String sql = "INSERT INTO tb_amigos(IdAmigo,NomeAmigo,TelefoneAmigo,EmailAmigo) VALUES(?,?,?,?)";
@@ -88,21 +95,23 @@ public class AmigoDAO {
                 stmt.setString(2, objeto.getNomeAmigo());
                 stmt.setString(3, objeto.getTelefoneAmigo());
                 stmt.setString(4, objeto.getEmailAmigo());
-                
+
                 stmt.execute();
             }
 
             return true;
-        } catch (SQLException erro) {
-            System.out.println(Erro + erro);
-            throw new RuntimeException(erro);
+        } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Erro ao inserir amigo na Tabela", ex);
         }
+        return false;
     }
-    
+
     /**
      * Deleta um amigo do banco de dados.
+     *
      * @param IdAmgio é o id do amigo que será deletado.
-     * @return True, caso seja possivel deletar, retornando um erro caso não seja possivel.
+     * @return True, caso seja possivel deletar, retornando um erro caso não
+     * seja possivel.
      */
     public boolean deleteAmigoBD(int IdAmigo) {
         try {
@@ -110,18 +119,18 @@ public class AmigoDAO {
                 stmt.executeUpdate("DELETE FROM tb_amigos WHERE IdAmigo = " + IdAmigo);
             }
 
-        } catch (SQLException erro) {
-            System.out.println(Erro + erro);
+         } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Erro ao deletar amigo na Tabela", ex);
         }
+        
         return true;
     }
-    
+
     /*
     Recupera um amigo do nosso DB, procurando ele pelo Id.
     @param id é o id do amigo a ser  recuperado.
     @return o amigo recuperado com base no Id.
-    */
-    
+     */
     public Amigo RecuperaAmigoDB(int id) {
         Amigo amigo = new Amigo();
         amigo.setIdAmigo(id);
@@ -132,18 +141,20 @@ public class AmigoDAO {
                 amigo.setNomeAmigo(res.getString("NomeAmigo"));
                 amigo.setTelefoneAmigo(res.getString("TelefoneAmigo"));
             }
-        } catch (SQLException erro) {
-            System.out.println(Erro + erro);
+        } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Erro ao achar amigo na Tabela", ex);
+        
         }
         return amigo;
-        
+
     }
-    
-    
+
     /**
      * Atualiza as informações de um amigo já existente no banco de dados.
+     *
      * @param objeto é o objeto do amigo, o qual será atualizado.
-     * @return True, caso seja possivel atualizar, retornando um erro caso não seja possivel.
+     * @return True, caso seja possivel atualizar, retornando um erro caso não
+     * seja possivel.
      */
     public boolean updateAmigoBD(Amigo objeto) {
 
@@ -155,15 +166,15 @@ public class AmigoDAO {
                 stmt.setString(2, objeto.getTelefoneAmigo());
                 stmt.setString(3, objeto.getEmailAmigo());
                 stmt.setInt(4, objeto.getIdAmigo());
-                
+
                 stmt.execute();
             }
 
             return true;
 
-        } catch (SQLException erro) {
-            System.out.println(Erro + erro);
-            throw new RuntimeException(erro);
+         } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Erro ao atualizar amigo na Tabela", ex);
         }
+        return false;
     }
 }
