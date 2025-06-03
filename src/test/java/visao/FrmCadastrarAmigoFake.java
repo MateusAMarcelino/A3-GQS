6e5919ba5b4c59b9aa94e8b2a37df2c464cfc3a1
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2025-06-03 17:20:26 -0300

    add : Feito teste do Cadastrar amigo

--- src/test/java/visao/FrmCadastrarAmigoFake.java
+++ src/test/java/visao/FrmCadastrarAmigoFake.java
@@ -5,39 +5,47 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FrmCadastrarAmigoFake extends FrmCadastrarAmigo {
-     /**
-     * Inicializa os atributos.
-     */
-    
+
     private static final Logger LOGGER = Logger.getLogger(FerramentaDAO.class.getName());
-    
+
+    private String mensagem;
+
     public FrmCadastrarAmigoFake() {
         super();
     }
-    
-    public void mostrarmensagem(String Mensagem) {
-        setMensagem(Mensagem);
-        
-         LOGGER.log(Level.WARNING, "Mensagem :" + Mensagem);
+
+    protected void mostrarMensagem(String mensagem) {
+        this.mensagem = mensagem;
+        LOGGER.log(Level.WARNING, "Mensagem: {0}", mensagem);
     }
-    
+
+    // Método para recuperar a última mensagem mostrada (para testes)
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    // Métodos auxiliares para os testes automatizados
     public javax.swing.JTextField getJTFNomedoAmigo() {
         return super.getJTFNome();
     }
-    
+
     public javax.swing.JTextField getJTFTelefone() {
         return super.getJTFTelefone();
     }
-    
+
     public javax.swing.JTextField getJTFEmail() {
         return super.getJTFEmail();
     }
-    
-     public void clicarBotaoCadastrar() {
+
+    public void clicarBotaoCadastrar() {
         super.getJBCadastrar().doClick();
     }
-     
-      public void clicarBotaoCacelar() {
+
+    public void clicarBotaoCancelar() {
         super.getJBCancelar().doClick();
     }
 }
--- src/test/java/visao/TesteFrmCadastrarAmigo.java
+++ src/test/java/visao/TesteFrmCadastrarAmigo.java
@@ -1,5 +1,9 @@
 package visao;
 
+import dao.AmigoDAO;
+import java.awt.Window;
+import javax.swing.JDialog;
+import modelo.Amigo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,26 +15,127 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author guiho
  */
-public class TesteFrmCadastrarAmigo {
-    
-    public TesteFrmCadastrarAmigo() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+class TesteFrmCadastrarAmigo {
+
+    Amigo testeamigo;
+    Amigo testeamigofake;
+
+    FrmCadastrarAmigoFake frmcadastraramigo;
+
     @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+    void inicializar() {
+
+        testeamigo = new Amigo(1, "Marlon", "123456789", "testeemail@gmail.com");
+        testeamigofake = new Amigo(1, "M", "1289", "testeemai");
+
+        frmcadastraramigo = new FrmCadastrarAmigoFake();
     }
 
-   
+    @Test
+    void testarcadastro() {
+
+        frmcadastraramigo.getJTFNome().setText(testeamigo.getNomeAmigo());
+        frmcadastraramigo.getJTFTelefone().setText(testeamigo.getTelefoneAmigo());
+        frmcadastraramigo.getJTFEmail().setText(testeamigo.getEmailAmigo());
+
+        frmcadastraramigo.clicarBotaoCadastrar();
+
+        assertEquals("Amigo inserido com sucesso!", frmcadastraramigo.getMensagem());
     }
+
+    @Test
+    void testCadastroAmigoInvalido() {
+        // Preenche os campos do formulário via getters do fake
+        frmcadastraramigo.getJTFNome().setText(testeamigofake.getNomeAmigo());
+        frmcadastraramigo.getJTFTelefone().setText(testeamigo.getTelefoneAmigo());
+        frmcadastraramigo.getJTFEmail().setText(testeamigo.getEmailAmigo());
+
+
+        // Fecha a tela
+        new javax.swing.Timer(500, e -> {
+            for (Window w : Window.getWindows()) {
+                if (w.isShowing() && w instanceof JDialog) {
+                    w.dispose(); // Fecha o JOptionPane como se clicasse em "OK"
+                }
+            }
+        }).start();
+
+        // Simula o clique no botão cadastrar via método do fake
+        frmcadastraramigo.clicarBotaoCadastrar();
+
+        // Verifica se a mensagem exibida é a de sucesso
+        assertEquals("O nome deve possuir ao menos 2 caracteres", frmcadastraramigo.getMensagem());
+    }
+
+    @Test
+    void testCadastroTefoneInvalido() {
+        // Preenche os campos do formulário via getters do fake
+        frmcadastraramigo.getJTFNome().setText(testeamigo.getNomeAmigo());
+        frmcadastraramigo.getJTFTelefone().setText(testeamigofake.getTelefoneAmigo());
+        frmcadastraramigo.getJTFEmail().setText(testeamigo.getEmailAmigo());
+
+        
+        // Fecha a tela
+        new javax.swing.Timer(500, e -> {
+            for (Window w : Window.getWindows()) {
+                if (w.isShowing() && w instanceof JDialog) {
+                    w.dispose(); // Fecha o JOptionPane como se clicasse em "OK"
+                }
+            }
+        }).start();
+
+        // Simula o clique no botão cadastrar via método do fake
+        frmcadastraramigo.clicarBotaoCadastrar();
+
+        // Verifica se a mensagem exibida é a de sucesso
+        assertEquals("O número de telefone deve possuir exatamente 9 digitos", frmcadastraramigo.getMensagem());
+
+    }
+    
+     @Test
+    void testCadastroEmailInvalido() {
+        // Preenche os campos do formulário via getters do fake
+        frmcadastraramigo.getJTFNome().setText(testeamigo.getNomeAmigo());
+        frmcadastraramigo.getJTFTelefone().setText(testeamigo.getTelefoneAmigo());
+         frmcadastraramigo.getJTFEmail().setText(testeamigofake.getEmailAmigo());
+
+
+        // Fecha a tela
+        new javax.swing.Timer(500, e -> {
+            for (Window w : Window.getWindows()) {
+                if (w.isShowing() && w instanceof JDialog) {
+                    w.dispose(); // Fecha o JOptionPane como se clicasse em "OK"
+                }
+            }
+        }).start();
+
+        // Simula o clique no botão cadastrar via método do fake
+        frmcadastraramigo.clicarBotaoCadastrar();
+
+        // Verifica se a mensagem exibida é a de sucesso
+        assertEquals("O email deve conter no mínimo 11 dígitos, como: X@gmail.com", frmcadastraramigo.getMensagem());
+
+    }
+
+    @Test
+    void testLimparCadastro() {
+        // Preenche os campos do formulário via getters do fake
+        frmcadastraramigo.getJTFNome().setText(testeamigo.getNomeAmigo());
+        frmcadastraramigo.getJTFTelefone().setText(testeamigo.getTelefoneAmigo());
+
+        // Simula o clique no botão Limpar via método do fake
+        frmcadastraramigo.clicarBotaoCancelar();
+
+        // Verifica se foi limpo os campos do formulário
+        assertEquals("", frmcadastraramigo.getJTFNome().getText());
+        assertEquals("", frmcadastraramigo.getJTFTelefone().getText());
+        assertEquals("", frmcadastraramigo.getJTFEmail().getText());
+        
+    }
+
+    @AfterEach
+    void finaliza() {
+        AmigoDAO amigodao = new AmigoDAO();
+        amigodao.deleteAmigoBD(1);
+    }
+}
