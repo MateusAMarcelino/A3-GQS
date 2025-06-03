0daeb5a9879cc92a073ef96097c4b63572d42d6e
mitsuo <guihonda10@gmail.com>
2025-06-03 09:46:07 -0300

    Teste: iniciando o teste do menu principal

---
+++ src/test/java/visao/TesteFrmMenuPrincipal.java
@@ -0,0 +1,33 @@
+package visao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TesteFrmMenuPrincipal {
+    
+    public TesteFrmMenuPrincipal() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+}
