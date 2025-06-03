06bcfd78ca0fb90b0fa0f965b1ecf6e5c2e22faf
mitsuo <guihonda10@gmail.com>
2025-06-03 09:33:26 -0300

    Fix: revomendo itens para ver se funciona

--- src/test/java/visao/TesteFrmCadastrarAmigo.java
+++ src/test/java/visao/TesteFrmCadastrarAmigo.java
@@ -32,16 +32,5 @@ public class TesteFrmCadastrarAmigo {
     public void tearDown() {
     }
 
-    /**
-     * Test of main method, of class FrmCadastrarAmigo.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        FrmCadastrarAmigo.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+   
     }
-    
-}
