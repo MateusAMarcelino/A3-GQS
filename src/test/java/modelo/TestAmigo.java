7b383c997bde7e4b6519382302e960f2e77c7c67
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2025-06-03 20:33:24 -0300

    add : terminando o coverage do TestAmigo

--- src/test/java/modelo/TestAmigo.java
+++ src/test/java/modelo/TestAmigo.java
@@ -1,11 +1,11 @@
 package modelo;
 
-
 import dao.AmigoDAO;
 import dao.EmprestimoDAO;
 import dao.Utilitario;
 import java.sql.*;
 import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,36 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class TestAmigo {
-    
-	Amigo instancia = new Amigo();
-        AmigoFake fake = new AmigoFake();
-        Amigo amigo = new Amigo(fake);
-    
+
+    Amigo instancia = new Amigo();
+    AmigoFake fake = new AmigoFake();
+    Amigo amigo = new Amigo(fake);
+
     /**
      * Testa o construtor sem argumentos do Amigo.
      */
     @Test
-     void testAmigo(){
+    void testAmigo() {
         assertTrue((instancia.getIdAmigo() == 0) && "".equals(instancia.getNomeAmigo()) && "".equals(instancia.getTelefoneAmigo()) && "".equals(instancia.getEmailAmigo()));
     }
-    
+
     /**
      * Testa os getters e setters do Amigo.
      */
     @Test
-     void testIdAmigoInt(){
+    void testIdAmigoInt() {
         instancia.setIdAmigo(1);
         instancia.setNomeAmigo("");
         instancia.setTelefoneAmigo("");
         instancia.setEmailAmigo("");
         assertTrue((instancia.getIdAmigo() == 1) && "".equals(instancia.getNomeAmigo()) && "".equals(instancia.getTelefoneAmigo()) && "".equals(instancia.getEmailAmigo()));
     }
-    
+
     /**
      * Testa a ArrayList do Amigo utilizando a classe do AmigoFake
      */
     @Test
-     void testListaAmigo(){
+    void testListaAmigo() {
         Amigo amigo = new Amigo(new AmigoFake());
         ArrayList<Amigo> lista = amigo.ListaAmigo();
 
@@ -50,7 +50,7 @@ class TestAmigo {
         assertEquals(1, lista.size());
         assertEquals("Osmar", lista.get(0).getNomeAmigo());
     }
-    
+
     /**
      * Testa o método insertAmigoBD do Amigo utilizando a classe do AmigoFake
      */
@@ -70,15 +70,15 @@ class TestAmigo {
      * Testa o método deleteAmigoBD do Amigo utilizando a classe do AmigoFake
      */
     @Test
-    void testDeleteAmigoBD(){
-        
+    void testDeleteAmigoBD() {
+
         boolean resultado = amigo.deleteAmigoBD(99);
-        
+
         assertTrue(resultado);
         assertTrue(fake.foiChamado);
         assertEquals(99, fake.ultimoIdDeletado);
     }
-    
+
     /**
      * Testa o método updateAmigoBD do Amigo utilizando a classe do AmigoFake
      */
@@ -95,7 +95,7 @@ class TestAmigo {
         assertEquals("Osmar@gmail.com", fake.ultimoAmigoAtualizado.getEmailAmigo());
         assertEquals(1, fake.ultimoAmigoAtualizado.getIdAmigo());
     }
-    
+
     /**
      * Testa o método recuperaAmigoDB do Amigo utilizando a classe do AmigoFake
      */
@@ -109,9 +109,9 @@ class TestAmigo {
         assertEquals("Osmar", resultado.getNomeAmigo());
         assertEquals("123456", resultado.getTelefoneAmigo());
         assertEquals("Osmar@gmail.com", resultado.getEmailAmigo());
-        assertEquals(1, fake.idRecebido); 
+        assertEquals(1, fake.idRecebido);
     }
-    
+
     /**
      * Testa o método procuraIndice do Amigo
      */
@@ -129,43 +129,51 @@ class TestAmigo {
         assertEquals(0, indiceEncontrado);
         assertEquals(-1, indiceNaoEncontrado);
     }
-    
+
     /**
      * Testa o método maiorId
      */
     @Test
     void testMaiorID() {
         int maiorId = instancia.MaiorID();
-        
+
         assertTrue(maiorId >= 0);
     }
-    
+
     /**
-     * Testa o método possuiEmprestimoAtivo inserindo um empréstimo ao banco de dados e o deletando no final
+     * Testa o método possuiEmprestimoAtivo inserindo um empréstimo ao banco de
+     * dados e o deletando no final
      */
     @Test
     void testPossuiEmprestimoAtivo() {
         Amigo amigo = new Amigo(2, "Osmar", "123456", "Osmar@gmail.com");
         Emprestimo emp = new Emprestimo(1000, 2, 1, "21-01-2025", null);
         EmprestimoDAO dao = new EmprestimoDAO();
-        
+
         //Insere o empréstimo criado ao banco de dados
         dao.insertEmprestimoBD(emp);
-        
+
         boolean resultado = amigo.possuiEmprestimoAtivo(2);
-        
+
         assertTrue(resultado);
-        
-        //Deleta o empréstimo criado pelo teste do banco de dados
-        try {
-            Connection conexao = new Utilitario().getConexao();
-            Statement stmt = conexao.createStatement();
-            stmt.executeUpdate("DELETE FROM tb_emprestimo WHERE IdEmprestimo = 1000");
-            stmt.close();
-        }   catch (SQLException e) {
-                e.printStackTrace();
-            }
+
     }
-    
-    
+
+    @Test
+    void testQuantidadeEmprestimo() {
+        Emprestimo emp = new Emprestimo();
+        emp.insertEmprestimoBD(1, 1, "05-05-2005");
+        int quantidadeEsperada = 1;
+        int quantidadeRecebida = amigo.quantidadeEmprestimo(1);
+        assertEquals(quantidadeEsperada, quantidadeRecebida);
+        emp.deleteEmprestimoBD(1);
+    }
+
+    @AfterEach
+    void limpar() {
+        EmprestimoDAO dao = new EmprestimoDAO();
+        dao.deleteEmprestimoBD(1000);
+        dao.deleteEmprestimoBD(1);
+    }
+
 }
