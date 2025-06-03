13634532a6396d6c1a73210d90e3db875507987b
mitsuo <guihonda10@gmail.com>
2025-06-03 09:42:18 -0300

    Teste: iniciando os atributos

--- src/test/java/visao/FrmCadastrarAmigoFake.java
+++ src/test/java/visao/FrmCadastrarAmigoFake.java
@@ -1,5 +1,11 @@
 package visao;
 
 public class FrmCadastrarAmigoFake extends FrmCadastrarAmigo {
+     /**
+     * Inicializa os atributos.
+     */
+    public FrmCadastrarAmigoFake() {
+        super();
+    }
     
 }
