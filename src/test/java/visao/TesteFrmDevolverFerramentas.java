3ecdd49e590eab19fbf944a4aca9665db376eb04
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-04 19:25:51 -0300

    test: começando o teste do FrmDevolverFerramentas

--- src/test/java/visao/TesteFrmDevolverFerramentas.java
+++ src/test/java/visao/TesteFrmDevolverFerramentas.java
@@ -1,5 +1,9 @@
 package visao;
 
+import dao.EmprestimoDAO;
+import modelo.Amigo;
+import modelo.Emprestimo;
+import modelo.Ferramenta;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,23 +13,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TesteFrmDevolverFerramentas {
     
-    public TesteFrmDevolverFerramentas() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    Amigo amigoteste;
+    Ferramenta ferramentateste;
+    Emprestimo emprestimoteste;
+    FrmDevolverFerramentasFake frmDevolverFerramentas;
     
     @BeforeEach
     public void setUp() {
+        amigoteste = new Amigo(1, "Osmar", "123456789", "Osmar@gmail.com");
+        ferramentateste = new Ferramenta(1, "Chave de fenda", "Tramontina", 9.50);
+        emprestimoteste = new Emprestimo();
+        
+        frmDevolverFerramentas = new FrmDevolverFerramentasFake();
     }
     
     @AfterEach
     public void tearDown() {
     }
     
+    @Test
+    void DevolucaoValida(){
+        amigoteste.insertAmigoBD("Osmar", "123456789", "Osmar@gmail.com");
+        ferramentateste.InsertFerramentaDB("Chave de fenda", "Tramontina", 9.50);
+        emprestimoteste.insertEmprestimoBD(1, 1, "21-01-2025");
+        
+        frmDevolverFerramentas.inicializar();
+        
+        frmDevolverFerramentas.getJCBEmprestimo().setSelectedIndex(0);
+        frmDevolverFerramentas.getJCBTipoRegistro().setSelectedIndex(0);
+        
+        frmDevolverFerramentas.clicarBotaoCadastrar();
+        
+        assertEquals("Devolucao cadastrada com sucesso", frmDevolverFerramentas.getMensagem());
+    }
 }
