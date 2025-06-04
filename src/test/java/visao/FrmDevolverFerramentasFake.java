a8eb50187137ea3df2a3c0259b51eb2b5319d86a
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-04 19:25:34 -0300

    test: começando o teste do FrmDevolverFerramentas

--- src/test/java/visao/FrmDevolverFerramentasFake.java
+++ src/test/java/visao/FrmDevolverFerramentasFake.java
@@ -1,5 +1,33 @@
 package visao;
 
+import javax.swing.*;
+
 public class FrmDevolverFerramentasFake extends FrmDevolverFerramentas {
     
+    public FrmDevolverFerramentasFake() {
+        super();
+    }
+
+    public void mostrarMensagem(String mensagem) {
+
+        setMensagem(mensagem);
+        
+        System.out.println("Mensagem:" + mensagem);
+    }
+
+    public void inicializar() {
+        carregaCBEmprestimo();
+    }
+
+    public void clicarBotaoCadastrar() {
+        super.getJBCadastrar().doClick();
+    }
+
+    public JComboBox<String> getJCBEmprestimo() {
+        return super.getJCBEmprestimo();
+    }
+
+    public JComboBox<String> getJCBTipoRegistro() {
+        return super.getJCBTipoRegistro();
+    }
 }
