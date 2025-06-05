fef4a5fd907fa1be23fa707d141eb610b147ead3
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-04 21:05:52 -0300

    test: começando o teste do FrmGerenciadorAmigo

--- src/test/java/visao/FrmGerenciadorAmigoFake.java
+++ src/test/java/visao/FrmGerenciadorAmigoFake.java
@@ -1,5 +1,35 @@
 package visao;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class FrmGerenciadorAmigoFake extends FrmGerenciadorAmigo {
     
+    public FrmGerenciadorAmigoFake() {
+        super();
+        DefaultTableModel modelo = new DefaultTableModel(new String[]{"ID", "Nome", "Telefone", "Email"}, 0);
+        JTable tabelaFalsa = new JTable(modelo);
+        
+        setJTableAmigos(tabelaFalsa);
+    }
+
+    public void mostrarMensagem(String mensagem) {
+
+        setMensagem(mensagem);
+        
+        System.out.println("Mensagem:" + mensagem);
+    }
+
+    public void inicializar() {
+        carregaTabela();
+    }
+
+    public void clicarBotaoJBApagar() {
+        super.getJBApagar().doClick();
+    }
+
+    public void clicarBotaoJBEditar() {
+        super.getJBEditar().doClick();
+        getJTableAmigos().setRowSelectionInterval(0, 0);
+    }
 }
