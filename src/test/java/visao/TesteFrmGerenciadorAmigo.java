44c71dab66b98da92fc11cd5c9b8176c7e66ab01
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-04 21:06:05 -0300

    test: começando o teste do FrmGerenciadorAmigo

--- src/test/java/visao/TesteFrmGerenciadorAmigo.java
+++ src/test/java/visao/TesteFrmGerenciadorAmigo.java
@@ -1,32 +1,46 @@
 package visao;
 
+import modelo.Amigo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import modelo.AmigoFake;
 
 public class TesteFrmGerenciadorAmigo {
     
-    public TesteFrmGerenciadorAmigo() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    AmigoFake fake = new AmigoFake();;
+    Amigo amigoteste = new Amigo(fake);
+    FrmGerenciadorAmigoFake frmGerenciarAmigo;
     
     @BeforeEach
     public void setUp() {
+        amigoteste = new Amigo(1, "Osmar", "123456789", "Osmar@gmail.com");
+        
+        frmGerenciarAmigo = new FrmGerenciadorAmigoFake();
+    }
+    
+    @Test
+    void EditarAmigo(){
+        amigoteste.insertAmigoBD("Osmar", "123456789", "Osmar@gmail.com");
+        
+        frmGerenciarAmigo.inicializar();
+        frmGerenciarAmigo.getJTableAmigos().setRowSelectionInterval(0, 0);
+        
+        Amigo novo = new Amigo(1, "Osmarzinho", "987654321", "Osmarzinho@gmail.com");
+        Boolean amigonovo = fake.updateAmigoBD(novo);
+        frmGerenciarAmigo.clicarBotaoJBEditar();
+        
+        assertTrue(amigonovo);
+        assertEquals("Osmarzinho", fake.ultimoAmigoAtualizado.getNomeAmigo());
+        assertEquals("987654321", fake.ultimoAmigoAtualizado.getTelefoneAmigo());
+        assertEquals("Osmarzinho@gmail.com", fake.ultimoAmigoAtualizado.getEmailAmigo());
+        assertEquals(1, fake.ultimoAmigoAtualizado.getIdAmigo());
     }
     
     @AfterEach
     public void tearDown() {
     }
-
-   
 }
