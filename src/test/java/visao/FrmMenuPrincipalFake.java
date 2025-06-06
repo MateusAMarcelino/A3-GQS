9504648a1ec70fb99d08b9ddb3abd9edc6ae0e46
mitsuo <guihonda10@gmail.com>
2025-06-06 00:08:54 -0300

    Test: arrumando o Menu Principal Fake

--- src/test/java/visao/FrmMenuPrincipalFake.java
+++ src/test/java/visao/FrmMenuPrincipalFake.java
@@ -1,10 +1,16 @@
 package visao;
 
 public class FrmMenuPrincipalFake extends FrmMenuPrincipal {
-        public boolean encerramentoChamado = false;
-        
-        @Override
-        protected void encerrarAplicacao() {
-            encerramentoChamado = true;
-        }
+
+    private boolean encerrarChamado = false;
+
+    @Override
+    protected void encerrarAplicacao() {
+        // Simula a chamada sem encerrar a aplicação
+        encerrarChamado = true;
     }
+
+    public boolean isEncerrarChamado() {
+        return encerrarChamado;
+    }
+}
