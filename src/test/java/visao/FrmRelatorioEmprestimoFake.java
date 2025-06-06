7c552a909e3761ba6a0cf2de3d51c0557ad3ea01
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-06 18:11:08 -0300

    test: começando o teste do FrmRelatorioEmpresitmo

--- src/test/java/visao/FrmRelatorioEmprestimoFake.java
+++ src/test/java/visao/FrmRelatorioEmprestimoFake.java
@@ -2,6 +2,38 @@ package visao;
 
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 
 public class FrmRelatorioEmprestimoFake extends FrmRelatorioEmprestimo {
+    
+    public FrmRelatorioEmprestimoFake(){
+        super();
+    }
+    
+    @Override
+    public void mostrarMensagem(String mensagem) {
+        //Atribui a mensagem ao atributo para ser utilizado nos testes
+        setMensagem(mensagem);
+
+        //Mostra a mensagem
+        System.out.println("Mensagem:" + mensagem);
+    }
+    
+    public void inicializarTabela(){
+        CarregaListaEmprestimo();
+    }
+    
+    public void selecionarLinha(int linha) {
+        JTable tabela = this.getJTableEmprestimosAtivos();
+        if (linha >= 0 && linha < tabela.getRowCount()) {
+            tabela.setRowSelectionInterval(linha, linha);
+
+            String id = tabela.getValueAt(linha, 0).toString();
+            getJLId().setText(id);
+        }
+    }
+    
+    public void clicarBotaoApagar() {
+        super.getJBApagar().doClick();
+    }
 }
