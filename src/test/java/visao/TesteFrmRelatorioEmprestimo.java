ec2c54544ebd28ca9ac1a2afbcd25585909472dc
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-06-06 18:10:50 -0300

    test: começando o teste do FrmRelatorioEmpresitmo

--- src/test/java/visao/TesteFrmRelatorioEmprestimo.java
+++ src/test/java/visao/TesteFrmRelatorioEmprestimo.java
@@ -1,5 +1,10 @@
 package visao;
 
+import dao.AmigoDAO;
+import dao.FerramentaDAO;
+import modelo.Amigo;
+import modelo.Emprestimo;
+import modelo.Ferramenta;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,23 +14,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TesteFrmRelatorioEmprestimo {
     
+    Emprestimo emp;
+    Amigo migo;
+    Ferramenta fer;
+    
+    FrmRelatorioEmprestimoFake frmRelatorioEmprestimo;
+    
     public TesteFrmRelatorioEmprestimo() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
     }
     
     @BeforeEach
     public void setUp() {
+        
+        emp = new Emprestimo(1, 1, 1, "2025-01-21", "FERRAMENTA NUNCA DEVOLVIDA");
+        migo = new Amigo(1, "Osmar", "123456789", "Osmarzinho@gmail.com");
+        fer = new Ferramenta(1, "Chave de fenda", "Tramontina", 7.50);
+        
+        frmRelatorioEmprestimo = new FrmRelatorioEmprestimoFake();
+    }
+    
+    @Test
+    void testCancelarFechaJanela() {
+        FrmRelatorioEmprestimo janela = new FrmRelatorioEmprestimo();
+
+        assertTrue(janela.isDisplayable());
+
+        janela.simularCliqueCancelar();
+
+        assertFalse(janela.isDisplayable());
+    }
+    
+    @Test
+    public void testDeletarEmprestimo(){
+        migo.insertAmigoBD("Osmar", "123456789", "Osmarzinho@gmail.com");
+        fer.InsertFerramentaDB("Chave de fenda", "Tramontina", 7.50);
+        emp.insertEmprestimoBD(1, 1, "2025-01-21");
+        
+        frmRelatorioEmprestimo.inicializarTabela();
+        frmRelatorioEmprestimo.selecionarLinha(0);
+        
+        frmRelatorioEmprestimo.clicarBotaoApagar();
+        
+        assertEquals("Emprestimo apagado com sucesso!", frmRelatorioEmprestimo.getMensagem());
     }
     
     @AfterEach
     public void tearDown() {
+        AmigoDAO amigodao = new AmigoDAO();
+        amigodao.deleteAmigoBD(1);
+        
+        FerramentaDAO ferramentadao = new FerramentaDAO();
+        ferramentadao.DeleteFerramentaDB(1);
     }
 
 }
